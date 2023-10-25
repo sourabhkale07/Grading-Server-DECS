00161ef37241d5c33096a02a96a1704776d9d042
sourabhkale07 <sourabhkale47@gmail.com>
2023-10-26 02:21:25 +0530

    Improved and added comments to the code

--- gradingClient.c
+++ gradingClient.c
@@ -6,6 +6,7 @@
 #include <netdb.h>
 #include <errno.h>
 
+// A Function to report errors and exit
 void reportError(char *msg)
 {
     perror(msg);
@@ -24,20 +25,25 @@ int main(int argc, char *arguments[])
     long fileSize;
     int timeoutCounter = 0;
 
+    // Check if the number of command line arguments is provided is correct?
     if (argc != 7)
     {
         fprintf(stderr, "usage %s <hostname>  <serverIP:port>  <sourceCodeFileTobeGraded>  <loopNum> <sleepTimeSeconds> <timeout-seconds>\n", arguments[0]);
         exit(0);
     }
 
+    //  convert the arguments
     portNumber = atoi(arguments[2]);      // Second arg is port no.
     int loopCount = atoi(arguments[4]);
     int sleepTime = atoi(arguments[5]);
     int successfulRequests = atoi(arguments[4]);
 
+    // Creating the socket
     socketFileDescriptor = socket(AF_INET, SOCK_STREAM, 0);
     if (socketFileDescriptor < 0)
         reportError("ERROR opening socket");
+
+    // to the server host by name
 
     serverHost = gethostbyname(arguments[1]);
     if (serverHost == NULL)
@@ -46,30 +52,39 @@ int main(int argc, char *arguments[])
         exit(0);
     }
 
+    // Initializing  the server address struct
     bzero((char *)&serverAddress, sizeof(serverAddress));
     serverAddress.sin_family = AF_INET;
     bcopy((char *)serverHost->h_addr, (char *)&serverAddress.sin_addr.s_addr,
           serverHost->h_length);
     serverAddress.sin_port = htons(portNumber);
 
-    if (connect(socketFileDescriptor, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)
+
+    // Connecting  to the server
+    if (connect(socketFileDescriptor, (struct sockaddr *)&serverAddress, sizeof(serverAddress) < 0))
         reportError("ERROR connecting");
 
+    // Set  socket timeout
     struct timeval connectionTimeout;
     connectionTimeout.tv_sec = atoi(arguments[6]); // Set the timeout to X seconds
     connectionTimeout.tv_usec = 0;
     if (setsockopt(socketFileDescriptor, SOL_SOCKET, SO_RCVTIMEO, &connectionTimeout, sizeof(connectionTimeout)) < 0)
         perror("timeout");
 
+    // Initializing  time variables 
     gettimeofday(&loopStartTime, NULL);
     char temporaryBuffer[10000];
 
+    //  number of loop iterations to the server
     n = write(socketFileDescriptor, &loopCount, sizeof(int));
     if (n < 0)
         reportError("ERROR writing to socket");
 
+
+    //  Loop for the number of iterations
     for (int i = 0; i < loopCount; i++)
     {
+        // Initializing  temporary buffer and open the source file
         memset(temporaryBuffer, 0, sizeof(temporaryBuffer));
         FILE *sourceFile = fopen(arguments[3], "rb");
         if (sourceFile == NULL)
@@ -77,25 +92,25 @@ int main(int argc, char *arguments[])
             reportError("ERROR opening file");
         }
 
-        // Calculate the file size
+        // Calculate the size of file
         fseek(sourceFile, 0, SEEK_END);
         fileSize = ftell(sourceFile);
         rewind(sourceFile);
 
-        // Allocate memory for the buffer based on file size
+        // Allocating  memory for the buffer based on file size
         dataBuffer = (char *)malloc(fileSize + 1);
         if (dataBuffer == NULL)
         {
             reportError("ERROR allocating memory");
         }
 
-        // Read the entire file into the buffer
+        // Reading the entire file  into the buffer
         size_t bytesRead = fread(dataBuffer, 1, fileSize, sourceFile);
         dataBuffer[bytesRead] = '\0'; // Null-terminate the buffer
 
         fclose(sourceFile);
 
-        // Send the content of the file to the server
+        // Send all content of the file to the server
         gettimeofday(&startTime, NULL);
         n = write(socketFileDescriptor, dataBuffer, bytesRead);
         if (n < 0)
@@ -118,14 +133,15 @@ int main(int argc, char *arguments[])
         gettimeofday(&endTime, NULL);
         double responseTime = (endTime.tv_sec - startTime.tv_sec) + (endTime.tv_usec - startTime.tv_usec) / 1e6;
         totalResponseTime = totalResponseTime + responseTime;
-        temporaryBuffer[n] = '\0'; // Null-terminate the received data
+        temporaryBuffer[n] = '\0'; // Null-terminate the data received
         printf("Server response: %s\n", temporaryBuffer);
         if (i < loopCount - 1)
             sleep(sleepTime);
 
-        // Free allocated memory
+        // allocated the memory free
         free(dataBuffer);
     }
+    
 
     gettimeofday(&loopEndTime, NULL);
     timeoutCounter = loopCount - successfulRequests;
@@ -138,4 +154,3 @@ int main(int argc, char *arguments[])
 
     return 0;
 }
-
